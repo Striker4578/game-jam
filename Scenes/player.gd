1aadb6102bb6389a8323c31a8b66ae42b3fdba74
dazed5573 <dazed5573@gmail.com>
2026-07-22 19:17:37 -0400

    movement fix and anim

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -1,8 +1,8 @@
 extends CharacterBody2D
 
 
-const SPEED = 150.0
-const JUMP_VELOCITY = -350.0
+const SPEED = 100.0
+const JUMP_VELOCITY = -325.0
 
  # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -26,6 +26,19 @@ func _physics_process(delta: float) -> void:
 		animated_sprite_2d.flip_h = false
 	elif direction < 0:
 		animated_sprite_2d.flip_h = true
+	
+	#Play animations !!!
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite_2d.play("idle")
+		else:
+			animated_sprite_2d.play("run")
+	else:
+		animated_sprite_2d.play("jump")
+	
+	
+	
+	
 	
 	#Apply movement
 	if direction:
