34a89962a52d1cbc46dc07320c5475f6ab86b4b4
Striker4578 <coolcoder0410@gmail.com>
2026-07-23 17:31:44 -0400

    mores

--- Scripts/water_bucket.gd
+++ Scripts/water_bucket.gd
@@ -6,11 +6,13 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	if Global.level == 0:
-		print("win")
 		get_tree().call_deferred("change_scene_to_file", "res://Scenes/level_spike_hide.tscn")
 		Global.level += 1
 	elif Global.level == 1:
-		get_tree().call_deferred("change_scene_to_file", "res://Scenes/level_up_down.tscn")
+		get_tree().call_deferred("change_scene_to_file", "res://Scenes/level_first_box.tscn")
 		Global.level += 1
 	elif Global.level == 2:
+		get_tree().call_deferred("change_scene_to_file", "res://Scenes/level_up_down.tscn")
+		Global.level += 1
+	elif Global.level == 3:
 		get_tree().call_deferred("change_scene_to_file", "res://Scenes/level_base.tscn")
