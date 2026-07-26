13c9eda6cf36822f28975f6bfa2a6db884d84deb
Striker4578 <coolcoder0410@gmail.com>
2026-07-26 02:06:15 -0400

    EVERYTHINGG

--- Scenes/torch.gd
+++ Scenes/torch.gd
@@ -3,6 +3,7 @@ extends Area2D
 @onready var sprite_2d: Sprite2D = $Sprite2D
 @onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
 @onready var point_light_2d: PointLight2D = $PointLight2D
+@onready var sfx_jump: AudioStreamPlayer2D = $sfx_jump
 
 signal torch_lit
 
@@ -14,11 +15,12 @@ func _ready() -> void:
 	$PointLight2D.hide()
 	
 func _on_body_entered(body: Node2D) -> void:
-	print("TOUCH REGISTERED! Node Name: ", body.name, " | In Player Group? ", body.is_in_group("player"))
 	if body.is_in_group("player") and not is_lit:
 		is_lit = true
 		$AnimatedSprite2D.show()
+		sfx_jump.play()
 		$AnimatedSprite2D.play("fire")
 		$PointLight2D.show()
+
 		
 		torch_lit.emit()
--- spring.gd
+++ spring.gd
@@ -1,6 +1,8 @@
 extends Area2D
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 @export var jump_force: float = -500.0
+@onready var sfx_jump: AudioStreamPlayer2D = $sfx_jump
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	body_entered.connect(_on_body_entered)
@@ -15,3 +17,4 @@ func _on_body_entered(body: Node2D) -> void:
 	if body.has_method("spring_jump"):
 		body.spring_jump(jump_force)
 		$AnimatedSprite2D.play("bounce")
+		sfx_jump.play()
